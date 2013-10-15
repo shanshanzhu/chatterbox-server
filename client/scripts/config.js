17da4a76a66058cb4c44269217b8e64634b13fd9
Hack Reactor Students <github@hackreactor.com>
2013-10-14 17:00:34 -0700

    Initial commit: server responds to get requests.

---
+++ client/scripts/config.js
@@ -0,0 +1,10 @@
+//IGNORE THIS
+//
+if (!/(&|\?)username=/.test(window.location.search)) {
+  var newSearch = window.location.search;
+  if (newSearch !== '' & newSearch !== '?') {
+    newSearch += '&';
+  }
+  newSearch += 'username=' + (prompt('What is your name?') || 'anonymous');
+  window.location.search = newSearch;
+}
--- server/request-handler.js
+++ server/request-handler.js
@@ -5,5 +5,36 @@
  * node module documentation at http://nodejs.org/api/modules.html. */
 
 var handleRequest = function(request, response) {
+  console.log("Serving request type " + request.method + " for url " + request.url);
+  var statusCode = 200;
+  var headers = defaultCorsHeaders;
 
+  headers['Content-Type'] = "text/plain";
+
+  response.writeHead(statusCode, headers);
+
+
+   if (request.method === "OPTIONS") {
+    response.end();
+   }
+
+   var path = request.url;
+
+   if (path === "/chatrooms") {
+    data = JSON.stringify({results: messages});
+   } else {
+    data = JSON.stringify(["Go away."]);
+   }
+
+  response.end(data);
 };
+
+var defaultCorsHeaders = {
+  "access-control-allow-origin": "*",
+  "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "access-control-allow-headers": "content-type, accept",
+  "access-control-max-age": 10 // Seconds.
+};
+
+exports.handleRequest = handleRequest;
+var messages = [{username: "The Boss", text: "Get to work.", roomname: "lobby"}];
